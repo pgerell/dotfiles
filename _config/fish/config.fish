c3b258ca752359272af41623432cc4613b142958
Peter Gerell <peter@gerell.se>
2022-10-26 14:29:52 +0200

    Finally the correct fd is called for windows paths

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -4,4 +4,6 @@ end
 
 # Set in PATH to avoid cluttering fish_variables with expanded user name
 fish_add_path --path $HOME/dotfiles/bin
+set -x FZF_DEFAULT_COMMAND "$HOME/dotfiles/bin/fd --type f"
+
 
