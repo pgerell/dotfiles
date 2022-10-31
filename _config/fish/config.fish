98519c10c0467208490b3c7846306206452bf077
Peter Gerell <peter@gerell.se>
2022-10-31 12:11:40 +0100

    Avoid explicit reference to user name

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -5,5 +5,4 @@ end
 # Set in PATH to avoid cluttering fish_variables with expanded user name
 fish_add_path --path $HOME/dotfiles/bin
 set -x FZF_DEFAULT_COMMAND "$HOME/dotfiles/bin/fd --type f"
-
-
+set -x nvm_data "$HOME/.local/share/nvm"
